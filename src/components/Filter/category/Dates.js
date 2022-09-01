dafe10a3ad11e2859a58f2c895f14bdf011bb304
gilbu <gilbertbuix57@gmail.com>
2022-09-01 12:48:14 -0700

    fix accordion on filters

--- src/components/Filter/category/Dates.js
+++ src/components/Filter/category/Dates.js
@@ -4,18 +4,18 @@ const Dates = ({ updateDates, updatePageNumber }) => {
   let listOfDates = ['2022-01-01,2022-12-31', '2021-01-01,2021-12-31', '2020-01-01,2020-12-31', '2019-01-01,2019-12-31', '2018-01-01,2018-12-31', '2017-01-01,2017-12-31', '2016-01-01,2016-12-31', '2015-01-01,2015-12-31', '2014-01-01,2014-12-31','2013-01-01,2013-12-31']
   return (
     <div className="accordion-item">
-      <h2 className="accordion-header" id="headingOne">
+      <h2 className="accordion-header" id="headingFour">
         <button
-          className="accordion-button" type="button"
-          data-bs-toggle="collapes" data-bs-target="#collapseOne"
-          aria-expanded="true" aria-controls="collapseOne"
+          className="accordion-button collapsed" type="button"
+          data-bs-toggle="collapse" data-bs-target="#collapseFour"
+          aria-expanded="false" aria-controls="collapseFour"
         > Dates </button>
       </h2>
       <div
-        id="collapseOne" className="accordion-collapse collapse show"
-        aria-labelledby="headingOne" data-bs-parent="#accordionExample"
+        id="collapseFour" className="accordion-collapse collapse"
+        aria-labelledby="headingFour" data-bs-parent="#accordionExample"
       >
-        <div className="accordion-body d flex flex-wrap gap-3">
+        <div className="accordion-body d-flex flex-wrap gap-3">
           {/* TODO: replace with date picker */}
           {listOfDates.map((item, index) => (
             <FilterBTN
--- src/components/Filter/category/Metacritic.js
+++ src/components/Filter/category/Metacritic.js
@@ -4,18 +4,18 @@ const Metacritic = ({ updateMetacritic, updatePageNumber }) => {
   let listOfMetacriticScores = ["1,50", '51,60', '61,70', '71,80', '81,90', '91,100' ]
   return (
     <div className="accordion-item">
-      <h2 className="accordion-header" id="headingOne">
+      <h2 className="accordion-header" id="headingThree">
         <button
-          className="accordion-button" type="button"
-          data-bs-toggle="collapes" data-bs-target="#collapseOne"
-          aria-expanded="true" aria-controls="collapseOne"
+          className="accordion-button collapsed" type="button"
+          data-bs-toggle="collapse" data-bs-target="#collapseThree"
+          aria-expanded="false" aria-controls="collapseThree"
         > Metacritic </button>
       </h2>
       <div
-        id="collapseOne" className="accordion-collapse collapse show"
-        aria-labelledby="headingOne" data-bs-parent="#accordionExample"
+        id="collapseThree" className="accordion-collapse collapse"
+        aria-labelledby="headingThree" data-bs-parent="#accordionExample"
       >
-        <div className="accordion-body d flex flex-wrap gap-3">
+        <div className="accordion-body d-flex flex-wrap gap-3">
           {/* TODO: replace with min and max slider */}
           {listOfMetacriticScores.map((item, index) => (
             <FilterBTN
