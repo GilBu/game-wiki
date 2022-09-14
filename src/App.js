a4dbd3faa55858ad404e3f5bca47ebd10dded5a6
GilBu <gilbertx57@gmail.com>
2022-09-13 21:21:32 -0700

    add routes

--- src/App.js
+++ src/App.js
@@ -9,10 +9,29 @@ import Pagination from "./components/Pagination/Pagination";
 import Filter from "./components/Filter/Filter";
 import Navbar from "./components/Navbar/Navbar";
 import {rawg} from "./apiKeys";
-
-
+import Creators from "./Pages/Creators";
+import Developers from "./Pages/Developers";
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  return (
+    <Router>
+      <div className='App'>
+        <Navbar />
+      </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+
+        <Route path="/creators" element={<Creators />} />
+
+        <Route path="/developers" element={<Developers />} />
+      </Routes>
+    </Router>
+  );
+}
+
+
+const Home = () => {
 
   let listOfPlatforms = [
     "pc",
@@ -92,12 +111,12 @@ function App() {
   let date = new Date();
   let firstDay = new Date(date.getFullYear(), date.getMonth(), 1).toISOString().slice(0, 10);
   let lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).toISOString().slice(0, 10);
-  
+
   let [fetchedData, updateFetchedData] = useState([]);
   let { count, next, prev, results } = fetchedData;
 
   let pages = results ? Math.ceil(count / results.length) : 1
-  let info = { count, next, pages, prev}
+  let info = { count, next, pages, prev }
   let [pageNumber, updatePageNumber] = useState(1);
 
   let [search, setSearch] = useState("");
@@ -106,10 +125,10 @@ function App() {
   let [metacritic, updateMetacritic] = useState("") //80,100
   let [platforms, updatePlatforms] = useState([...listOfPlatforms.keys()])
   let [dates, updateDates] = useState("") //2022-01-01,2022-12-31
-  let [tags, updateTags] = useState("jrpg")   
-  
-  let api = `https://api.rawg.io/api/games?key=${rawg}&dates=${dates}&page=${pageNumber}&search=${search}&genres=${genres}&metacritic=${metacritic}&platforms=${platforms}&ordering=-metacritic`
-  
+  let [tags, updateTags] = useState("jrpg")
+
+  let api = `https://api.rawg.io/api/games?key=${rawg}&dates=${dates}&page=${pageNumber}&search=${search}&genres=${genres}&metacritic=${metacritic}&platforms=${platforms}&ordering=-metacritic&tags=${tags}`
+
   useEffect(() => {
     (async function () {
       let data = await fetch(api).then((res) => res.json());
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,51 @@
+import { NavLink, Link } from "react-router-dom";
+import "../../App.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
+      <div className="container">
+        <Link to="/" className="navbar-brand fs-3 ubuntu">
+          Game <span className="text-primary">Wiki</span>
+        </Link>
+        <style jsx>{`
+          button[aria-expanded="false"] > .close {
+            display: none;
+          }
+          button[aria-expanded="true" ] .open {
+            display: none;
+          }
+        `}</style>
+        <button
+          className="navbar-toggler border-0"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="fas fa-bars open text-dark"></span>
+          <span class="fas fa-times close text-dark"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNavAltMarkup"
+        >
+          <div className="navbar-nav fs-5">
+            <NavLink to="/" className="nav-link">
+              Games
+            </NavLink>
+            <NavLink to="/creators" className="nav-link">
+              Creators
+            </NavLink>
+            <NavLink to="developers" className="nav-link">
+              Developers
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
