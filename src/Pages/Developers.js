c1d6e436de74650cf0b22c3be07a1c2d33bda0db
gilbu <gilbertbuix57@gmail.com>
2022-09-28 20:54:08 -0700

    add Developers page

--- src/Pages/Developers.js
+++ src/Pages/Developers.js
@@ -2,23 +2,52 @@ import React, { useEffect, useState } from "react"
 import Card from "../components/Card/Card"
 import InputGroup from "../components/Filter/category/InputGroup"
 import { rawg } from "../apiKeys";
+import Search from "../components/Search/Search";
+import Pagination from "../components/Pagination/Pagination";
 
 const Developers = () => {
-  let [results, setResults] = useState([])
-  let [info, setInfo] = useState([])
-  let { name, slug, image_backgroud, game_count } = info
-  let [id, setID] = useState(1)
+  let [fetchedData, updateFetchedData] = useState([]);
+  let { count, next, prev, results } = fetchedData;
+  let pages = results ? Math.ceil(count / results.length) : 1
+  let info = { count, next, pages, prev }
+  let [pageNumber, updatePageNumber] = useState(1);
+  let [search, setSearch] = useState("");  let [id, setID] = useState(1)
+
   let api = `https://api.rawg.io/api/developers?key=${rawg}`
 
   useEffect(() => {
     (async function () {
-      let data = await fetch(api).then((res) => res.json())
-      setInfo(data)
-
-      
+      let data = await fetch(api).then((res) => res.json());
+      updateFetchedData(data)
       console.log(data)
-    })()
-  }, [api])
+    })();
+  }, [api]);
+
+  if (results) {
+    return (
+      <div className="App">
+      <h1 className="text-center mb-3">Developers</h1>
+      <Search setSearch={setSearch} updatePageNumber={updatePageNumber} />
+      <div className="container">
+        <div className="row">
+         
+          <div className="col-lg-8 col-12">
+            <div className="row">
+              <Card results={results} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Pagination
+        info={info}
+        pageNumber={pageNumber}
+        updatePageNumber={updatePageNumber}
+      />
+    </div>
+    )
+  } else{
+    return "Developers Not Found"
+  }
 }
 
 export default Developers
